fb1acf7feaa5eb014c4e935d05720203a6f39388
rcav8tr <ajb_flyer@yahoo.com>
2025-12-05 10:50:07 -0600

    Version 1.0.8

--- Systems/UISystem.cs
+++ Systems/UISystem.cs
@@ -269,7 +269,7 @@ namespace PerformanceMonitor
         /// <summary>
         /// Event callback when game saving is started or ended.
         /// </summary>
-        private void GameManager_onGameSaveLoad(string saveName, bool start, bool success)
+        private void GameManager_onGameSaveLoad(string saveName, string previewUri, bool start, bool success)
         {
             // Check for start or end of game save.
             if (start)
